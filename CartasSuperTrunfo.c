40895739e6341568b88bb0ef2d71ab5403f4dc37
LIMA2005-eng <ds13579lm20@gmail.com>
2025-06-02 17:31:51 +0000

    2.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,55 +1,45 @@
- #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <stdio.h>
+ 
+int main(){
 
-// Definição da estrutura para representar um país
-typedef struct {
-    char nome[50];
-    int area;
+    int estado[50];
+    int codigo[2];
+    int cidade[22];
     int populacao;
-    float idh;
-} Pais;
+    int Pontosturisticos;
+    float area;
+    float pib;
 
-// Função para criar uma carta (objeto Pais)
-Pais cria_carta(const char *nome, int area, int populacao, float idh) {
-    Pais carta;
-    strcpy(carta.nome, nome);
-    carta.area = area;
-    carta.populacao = populacao;
-    carta.idh = idh;
-    return carta;
-}
+    printf("Digite seu Estado: \n");
+    scanf("%s" , &estado);
 
-// Função para embaralhar as cartas
-void embaralha_cartas(Pais cartas[], int num_cartas) {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios com a hora atual
-    for (int i = 0; i < num_cartas; i++) {
-        int j = rand() % num_cartas;
-        Pais temp = cartas[i];
-        cartas[i] = cartas[j];
-        cartas[j] = temp;
-    }
-}
+    printf("Codigo: \n");
+    scanf("%s" , &codigo);
 
-// Função principal (simulando um jogo)
-int main() {
-    // Criando algumas cartas (países)
-    Pais cartas[2]; // Exemplo com 4 cartas
-    cartas[0] = cria_carta("Brasil", 8515767, 210000000, 0.76);
-    cartas[1] = cria_carta("Estados Unidos", 9833520, 330000000, 0.94);
+    printf("Nome da Cidade: \n");
+    scanf("%s" , &cidade);
+
+    printf("Total de Habitantes: \n");
+    scanf("%d" , &populacao);
     
+    printf("Area: \n");
+    scanf("%f" , &area);
 
-    // Embaralhando as cartas
-    embaralha_cartas(cartas, 2);
+    printf("Pib: \n");
+    scanf("%f", &pib);
 
-    // Exibindo as cartas embaralhadas (para verificar)
-    printf("Cartas Embaralhadas:\n");
-    for (int i = 0; i < 2; i++) {
-        printf("%s - Area: %d - Populacao: %d - IDH: %.2f\n", cartas[i].nome, cartas[i].area, cartas[i].populacao, cartas[i].idh);
-    }
 
-    // Implementação da lógica do jogo (em um próximo passo)
+    printf("Pontos turisticos: \n");
+    scanf("%d" , &Pontosturisticos);
 
+    printf("Carta 1! \n");
+    printf("Estado: %s \n" , estado);
+    printf("Codigo da carta: %s%s \n", estado, codigo);
+    printf("Cidade: %s \n", cidade);
+    printf("Habitantes: %d \n", populacao);
+    printf("Area: %f \n", area);
+    printf("Pib: %f \n", pib);
+    printf("Pontos turisticos: %d \n", Pontosturisticos);
+ 
     return 0;
 }
