9bb3f6c55d51e3aeadf72b4afad84286e94efacd
LIMA2005-eng <ds13579lm20@gmail.com>
2025-06-02 15:27:31 +0000

    1.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -35,18 +35,17 @@ void embaralha_cartas(Pais cartas[], int num_cartas) {
 // Função principal (simulando um jogo)
 int main() {
     // Criando algumas cartas (países)
-    Pais cartas[4]; // Exemplo com 4 cartas
+    Pais cartas[2]; // Exemplo com 4 cartas
     cartas[0] = cria_carta("Brasil", 8515767, 210000000, 0.76);
     cartas[1] = cria_carta("Estados Unidos", 9833520, 330000000, 0.94);
-    cartas[2] = cria_carta("China", 9706961, 1400000000, 0.76);
-    cartas[3] = cria_carta("Alemanha", 357022, 83000000, 0.94);
+    
 
     // Embaralhando as cartas
-    embaralha_cartas(cartas, 4);
+    embaralha_cartas(cartas, 2);
 
     // Exibindo as cartas embaralhadas (para verificar)
     printf("Cartas Embaralhadas:\n");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 2; i++) {
         printf("%s - Area: %d - Populacao: %d - IDH: %.2f\n", cartas[i].nome, cartas[i].area, cartas[i].populacao, cartas[i].idh);
     }
 
