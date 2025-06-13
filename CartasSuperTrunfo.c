06a4824998840cb8e770310cb184fe051d94863a
LIMA2005-eng <ds13579lm20@gmail.com>
2025-06-13 12:47:31 +0000

    5.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,95 +1,94 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
-    // carta 01
-    char estado1;
-    char codigocarta1[6];
-    char nomecidade1[50];
-    int populacao1, pontostur1;
-    float pib1, area_km1;
+// Estrutura representando um país
+typedef struct {
+    char nome[30];
+    int populacao; // em milhões
+    float area;    // em mil km²
+    float pib;     // em trilhões USD
+} Pais;
 
-     // carta 02
-     char estado2;
-     char codigocarta2[6];
-     char nomecidade2[50];
-     int populacao2, pontostur2;
-     float pib2, area_km2;
+// Função para mostrar os dados do país
+void mostrarPais(Pais p) {
+    printf("País: %s\n", p.nome);
+    printf("População: %d milhões\n", p.populacao);
+    printf("Área: %.2f mil km²\n", p.area);
+    printf("PIB: %.2f trilhões USD\n", p.pib);
+}
 
-     
-     // Corresponde a Carta 01
-    printf("Primeiramente iremos inserir as informações correspondentes a Carta 01!! \n");
+// Função para comparar os países com base em um atributo
+void comparar(Pais jogador, Pais computador, int atributo) {
+    printf("\n--- Comparando Países ---\n");
+    printf("\nVocê escolheu:\n");
+    mostrarPais(jogador);
 
-    printf("Digite uma letra maiúscula entre A e H (ela irá corresponder à carta 01): \n");
-    scanf(" %c", &estado1);
+    printf("\nOponente (computador):\n");
+    mostrarPais(computador);
 
-    printf("Digite o codigo da carta (sendo o código correspondente a letra inserida anteriormente + um numero de 01 a 04): \n");
-    scanf(" %s", &codigocarta1);
+    printf("\nResultado: ");
 
-    printf("Digite o nome da cidade que a carta corresponderá: \n");
-    scanf(" %s", &nomecidade1);
+    switch(atributo) {
+        case 1: // População
+            if (jogador.populacao > computador.populacao)
+                printf("Você venceu!\n");
+            else if (jogador.populacao < computador.populacao)
+                printf("Computador venceu!\n");
+            else
+                printf("Empate!\n");
+            break;
+        case 2: // Área
+            if (jogador.area > computador.area)
+                printf("Você venceu!\n");
+            else if (jogador.area < computador.area)
+                printf("Computador venceu!\n");
+            else
+                printf("Empate!\n");
+            break;
+        case 3: // PIB
+            if (jogador.pib > computador.pib)
+                printf("Você venceu!\n");
+            else if (jogador.pib < computador.pib)
+                printf("Computador venceu!\n");
+            else
+                printf("Empate!\n");
+            break;
+        default:
+            printf("Atributo inválido!\n");
+    }
+}
 
-    printf("Digite a quantidade da populacao dessa cidade: \n");
-    scanf(" %i", &populacao1);
+int main() {
+    Pais brasil = {"Brasil", 214, 8516.0, 2.0};
+    Pais alemanha = {"Alemanha", 83, 357.0, 4.5};
 
-    printf("Digite a area em km2 da cidade: \n");
-    scanf(" %f", &area_km1);
+    int escolhaPais, atributo;
+    Pais jogador, computador;
 
-    printf("Digite o pib da cidade: \n");
-    scanf(" %f", &pib1);
+    printf("=== Super Trunfo: Brasil x Alemanha ===\n");
+    printf("Escolha seu país:\n");
+    printf("1. Brasil\n");
+    printf("2. Alemanha\n");
+    printf("Digite 1 ou 2: ");
+    scanf("%d", &escolhaPais);
 
-    printf("Digite o numero de pontos turisticos da cidade: \n");
-    scanf(" %i", &pontostur1);
+    if (escolhaPais == 1) {
+        jogador = brasil;
+        computador = alemanha;
+    } else if (escolhaPais == 2) {
+        jogador = alemanha;
+        computador = brasil;
+    } else {
+        printf("Escolha inválida.\n");
+        return 1;
+    }
 
-    printf("OK! Você já inseriu os dados da carta 01.");
-    
-    // Corresponde a Carta 02
-    
-    printf("Agora iremos inserir as informações correspondentes a Carta 02!! \n");
+    printf("\nEscolha um atributo para competir:\n");
+    printf("1. População\n2. Área\n3. PIB\n");
+    printf("Digite 1, 2 ou 3: ");
+    scanf("%d", &atributo);
 
-    printf("Digite uma letra maiúscula entre A e H (ela irá corresponder à carta 02): \n");
-    scanf(" %c", &estado2);
+    comparar(jogador, computador, atributo);
 
-    printf("Digite o codigo da carta (sendo o código correspondente a letra inserida anteriormente + um numero de 01 a 04): \n");
-    scanf(" %s", &codigocarta2);
-
-    printf("Digite o nome da cidade que a carta corresponderá: \n");
-    scanf(" %s", &nomecidade2);
-
-    printf("Digite a quantidade da populacao dessa cidade: \n");
-    scanf(" %i", &populacao2);
-
-    printf("Digite a area em km2 da cidade: \n");
-    scanf(" %f", &area_km2);
-
-    printf("Digite o pib da cidade: \n");
-    scanf(" %f", &pib2);
-
-    printf("Digite o numero de pontos turisticos da cidade: \n");
-    scanf(" %i", &pontostur2);
-
-    printf("OK! Todas as informações ja foram preenchidas. As cartas correspondentes sao: \n");
-
-    // Carta 01
-
-    printf("Carta 01: \n");
-    printf("Estado: %c \n", estado1);
-    printf("Codigo: %s \n", codigocarta1);
-    printf("Nome da cidade: %s \n", nomecidade1);
-    printf("Quantidade da populacao: %i \n", populacao1);
-    printf("Area em km2: %f \n", area_km1);
-    printf("Pib da cidade: %f \n", pib1);
-    printf("Quantidade de pontos turisticos: %i \n", pontostur1);
-
-    // Carta 02
-
-    
-    printf("Carta 02: \n");
-    printf("Estado: %c \n", estado2);
-    printf("Codigo: %s \n", codigocarta2);
-    printf("Nome da cidade: %s \n", nomecidade2);
-    printf("Quantidade da populacao: %i \n", populacao2);
-    printf("Area em km2: %f \n", area_km2);
-    printf("Pib da cidade: %f \n", pib2);
-    printf("Quantidade de pontos turisticos: %i \n", pontostur2);
+    return 0;
 }
